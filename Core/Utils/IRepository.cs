8fafadc20b0c8e136a921cf26342751c76d7ca7b
aribilgiogr <aribilgiogr@gmail.com>
2026-07-19 11:25:05 +0300

    Update 01 19.07.2026

---
+++ Core/Utils/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using Core.Abstracts.Bases;
+using System.Linq.Expressions;
+
+namespace Core.Utils
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task CreateOneAsync(T entity);
+        Task CreateManyAsync(IEnumerable<T> entities);
+
+        Task<T?> ReadByIdAsync(object key); // key: Entity/Primary Key
+        Task<IEnumerable<T>> ReadManyAsync(Expression<Func<T, bool>>? where = null);
+
+        void UpdateOne(T entity);
+        void UpdateMany(IEnumerable<T> entities);
+
+        void DeleteOne(T entity);
+        void DeleteOne(object key);
+        void DeleteMany(IEnumerable<T> entities);
+        void DeleteMany(Expression<Func<T, bool>>? where = null);
+
+        Task<bool> AnyAsync(Expression<Func<T, bool>>? where = null);
+        Task<int> CountAsync(Expression<Func<T, bool>>? where = null);
+    }
+}
---
+++ Core/Utils/Repository.cs
@@ -0,0 +1,53 @@
+﻿using Core.Abstracts.Bases;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Core.Utils
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        protected readonly DbContext _context;
+        protected readonly DbSet<T> _set;
+
+        protected Repository(DbContext context)
+        {
+            _context = context;
+            _set = _context.Set<T>();
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<T, bool>>? where = null) => await _set.AnyAsync(where ?? (x => true));
+
+        public async Task<int> CountAsync(Expression<Func<T, bool>>? where = null) => await _set.CountAsync(where ?? (x => true));
+
+        public async Task CreateManyAsync(IEnumerable<T> entities) => await _set.AddRangeAsync(entities);
+
+        public async Task CreateOneAsync(T entity) => await _set.AddAsync(entity);
+
+        public void DeleteMany(IEnumerable<T> entities) => _set.RemoveRange(entities);
+
+        public void DeleteMany(Expression<Func<T, bool>>? where = null)
+        {
+            var entities = _set.Where(where ?? (x => true));
+            DeleteMany(entities);
+        }
+
+        public void DeleteOne(T entity) => _set.Remove(entity);
+
+        public void DeleteOne(object key)
+        {
+            var entity = _set.Find(key);
+            if (entity != null)
+            {
+                DeleteOne(entity);
+            }
+        }
+
+        public async Task<T?> ReadByIdAsync(object key) => await _set.FindAsync(key);
+
+        public async Task<IEnumerable<T>> ReadManyAsync(Expression<Func<T, bool>>? where = null) => await _set.Where(where ?? (x => true)).ToListAsync();
+
+        public void UpdateMany(IEnumerable<T> entities) => _set.UpdateRange(entities);
+
+        public void UpdateOne(T entity) => _set.Update(entity);
+    }
+}
