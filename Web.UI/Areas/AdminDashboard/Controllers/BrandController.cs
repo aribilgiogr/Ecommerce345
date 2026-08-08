8a65a499c5375acc3964004699e79c9d51b8e3f8
aribilgiogr <aribilgiogr@gmail.com>
2026-08-08 14:22:56 +0300

    Update 02 08.08.2026

---
+++ Web.UI/Areas/AdminDashboard/Controllers/BrandController.cs
@@ -0,0 +1,78 @@
+﻿using Core.Abstracts.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.UI.Areas.AdminDashboard.Controllers
+{
+    [Area("AdminDashboard")]
+    public class BrandController(IBrandService service) : Controller
+    {
+        // GET: BrandController
+        public async Task<ActionResult> Index()
+        {
+            return View(await service.GetBrandsAsync());
+        }
+
+        // GET: BrandController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BrandController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BrandController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BrandController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BrandController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BrandController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Web.UI/Areas/AdminDashboard/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.UI.Areas.AdminDashboard.Controllers
+{
+    [Area("AdminDashboard")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
